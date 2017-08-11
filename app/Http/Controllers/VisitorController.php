d321afc7205aff84247e1bd8f508d5d3844cd433
aungtnaing <aungtnaing82@gmail.com>
2017-08-11 19:07:47 +0630

    new weewdsfd

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -176,6 +176,17 @@ class VisitorController extends Controller {
 
     });
 
+    Mail::send('emails.layoutmail', $data, function ($message) use ($data){
+
+
+
+        $message->from('stieducontact@gmail.com', $data['email']);
+
+        $message->to('jedalyn@stiedu.net')->subject('New Visitor Enquiry')
+        										->replyTo($data['email']);
+
+    });
+
 
     	// var_dump($request->input("submittype"));
     	// die();
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -25,6 +25,17 @@ src="https://www.facebook.com/tr?id=450642131975254&ev=PageView&noscript=1"
 <!-- DO NOT MODIFY -->
 <!-- End Facebook Pixel Code -->
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-104412099-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 
   <link rel="apple-touch-icon" sizes="120x120" href="<?php echo url(); ?>/images/apple-touch-icon-120x120-precomposed.png" /> 
   <link rel="apple-touch-icon" sizes="152x152" href="<?php echo url(); ?>/images/apple-touch-icon-152x152-precomposed.png" />
