de05032527c2a3a4dced345025c210225990dd22
aungtnaing <aungtnaing82@gmail.com>
2017-08-30 19:05:27 +0630

    ewew

--- resources/views/pages/fbtest.blade.php
+++ resources/views/pages/fbtest.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <title>fbtest</title>
 
  
 <meta property="og:url"           content="http://www.stiedu.net/coursedetails/7" />
-  <meta property="og:type"          content="website" />
-  <meta property="og:title"         content="Your Website Title" />
-  <meta property="og:description"   content="Your description" />
+  <meta property="og:type"          content="article" />
+  <meta property="og:title"         content="BA ( Hons ) Business Administration" />
+  <meta property="og:description"   content="This course provides students who want to develop their management and business-planning knowledge and skills with an advanced, practical business education.It supports entry and progression in management careers in industry, commerce or the public sector and provides access to Masters and professional courses in specific are as such as Finance, HRM and marketing." />
   <meta property="og:image"         content="http://www.stiedu.net/images/courses/7/photos/1497605680-photo.JPG" />
 
   
@@ -25,5 +25,16 @@
 
   <!-- Your share button code -->
 <div class="fb-share-button" data-href="http://www.stiedu.net/coursedetails/7" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.stiedu.net%2Fcoursedetails%2F7&amp;src=sdkpreparse">Share</a></div>
+
+<a href="javascript:fbshareCurrentPage()" target="_blank" alt="Share on Facebook">Facebook</a>
+
+<script language="javascript">
+    function fbshareCurrentPage()
+    {window.open("https://www.facebook.com/sharer/sharer.php?u="+escape(window.location.href)+"&t="+document.title, '', 
+    'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+    return false; }
+</script>
+
 </body>
+
 </html>
