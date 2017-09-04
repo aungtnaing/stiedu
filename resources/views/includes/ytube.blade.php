dd0569de8a332ca5dd82eaa109d51f9e2b6f326b
aungtnaing <aungtnaing82@gmail.com>
2017-09-04 20:06:08 +0630

    newd ytube

---
+++ resources/views/includes/ytube.blade.php
@@ -0,0 +1,19 @@
+
+  <section id="" class="page">
+    <section class="section padding-bottom-off">
+        
+                <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <hgroup class="section-title align-center opacity">
+                        <h1 class="header-title" style="color:#050930;">STI MYANMAR UNIVERSITY</h1> 
+                    </hgroup>
+                </div>
+            </div>
+            <!--/ .row-->
+        </div>
+<iframe width="1280" height="720" src="https://www.youtube.com/embed/d-PlKr2e3Dg?autoplay=1" frameborder="0" allowfullscreen></iframe>
+    </section>
+  
+</section>
+       
