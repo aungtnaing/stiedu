6013744f6cbf981f34aa7363a15dcf46a9fce41c
aungtnaing <aungtnaing82@gmail.com>
2017-12-07 17:01:43 +0630

    new added

--- resources/views/includes/mainslides.blade.php
+++ resources/views/includes/mainslides.blade.php
@@ -5,7 +5,7 @@
             <div id="layerslider">
                 @foreach($mainslides as $mainslide)
                 
-                <div class="ls-slide" style="slidedirection: right; durationin: 1500; durationout: 1500; easingin: easeInOutQuint; timeshift: -500;"> <img alt="" class="ls-bg" src="<?php echo url(); ?>/{{ $mainslide->photourl1 }}">
+                <div class="ls-slide" style="slidedirection: right; durationin: 1500; durationout: 1500; easingin: easeInOutQuint; timeshift: -500;"> <img alt="" class="ls-bg" src="<?php echo url(); ?>{{ $mainslide->photourl1 }}">
                     <h1 class="ls-s2 align-center" style="top: 43%; left: 180px; slidedirection : top; slideoutdirection : fade; scaleout : 0.75; durationin : 2000; durationout : 1000; easingin : easeInOutQuint; easingout : easeInOutQuint;color:#fff6;">
                         {{ $mainslide->title }}
                     </h1>
@@ -13,7 +13,7 @@
                         {{ $mainslide->stitle }}
                         <br>
                         @if($mainslide->aherfdata!="")
-                         <a href="{{ $mainslide->aherfdata }}" class="btn btn-primary btn-lg">MORE DETAILS</a>
+                         <a href="<?php echo url(); ?>{{ $mainslide->aherfdata }}" class="btn btn-primary btn-lg">MORE DETAILS</a>
                          @endif
                     </h1> 
 
